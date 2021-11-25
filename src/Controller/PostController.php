10445ac58a255347c8d192b8de96bbaac9a48ec6
Rannie Ollit <rannie.ollit@petnet.com.ph>
2021-11-25 16:43:11 +0800

    WIP: Doctrine

---
+++ src/Controller/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/post", name="post.")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAll();
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     * @param Request $request
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    public function create(Request $request, PostRepository $postRepository): Response
+    {
+        $post = (new Post())->setTitle('sample title');
+        $entityManager = $postRepository->getEntityManager();
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return new Response("Post was created.");
+    }
+}
